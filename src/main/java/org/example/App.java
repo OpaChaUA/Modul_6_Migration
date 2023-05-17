acdfdf47d205b26374980d01314c0dcd1927b872
OpaChaUA <opaCha.ua.94@gmail.com>
2023-05-17 20:29:22 +0300

    fix, delete, delete, fix

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,12 +1,11 @@
 package org.example;
 
-import java.sql.Connection;
 
 public class App {
     public static void main(String[] args) {
         new FlayWayConfiguration().initDb();
 
-      ClientSevice clientService = new ClientSevice();
+        ClientSevice clientService = new ClientSevice();
         System.out.println(clientService.create("New_Client"));
         System.out.println(clientService.getById(3));
         clientService.deleteById(4);
