641719e0d9831014d98cadaa9e06614e4df0991c
OpaChaUA <opaCha.ua.94@gmail.com>
2023-05-17 20:14:55 +0300

    fix

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,17 @@
 package org.example;
 
-public class Main {
+import java.sql.Connection;
+
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Connection connection = Database.getInstance().getConnection();
+        new FlayWayConfiguration().initDb();
+
+      ClientSevice clientService = new ClientSevice();
+        System.out.println(clientService.create("New_Client"));
+        System.out.println(clientService.getById(3));
+        clientService.deleteById(4);
+        System.out.println(clientService.listAll());
+
     }
 }
